8d932620815f687c32eff404b423d3b55c1fac72
priyanka_27 <dalwadipriyanka1727@gmail.com>
2024-09-01 11:08:25 +0530

    user constructor communicate with database schema

---
+++ src/User.java
@@ -0,0 +1,15 @@
+public class User {
+    private String username;
+    private String password;
+    private boolean isAdmin;
+
+    public User(String username, String password, boolean isAdmin) {
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    
+
+   
+}
