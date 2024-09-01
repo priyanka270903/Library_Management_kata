47459f9760296fc0b343175edec9c6a03686c735
priyanka_27 <dalwadipriyanka1727@gmail.com>
2024-09-01 12:16:30 +0530

    Added main file as entery point of CLI Application

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        DatabaseHelper obj = new DatabaseHelper();
+        LibrarySystem system = new LibrarySystem();
+        system.start();
+    
+       
+    }
+}
