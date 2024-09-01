d57c27bca341c01e7c897b6cf6892a2138a1db8a
priyanka_27 <dalwadipriyanka1727@gmail.com>
2024-09-01 11:16:18 +0530

    book model setup

---
+++ src/Book.java
@@ -0,0 +1,51 @@
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+    private int publicationYear;
+
+    public Book(String isbn, String title, String author, int publicationYear) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ISBN: %s, Title: %s, Author: %s, Year: %d", isbn, title, author, publicationYear);
+    }
+}
+    
