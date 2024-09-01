a88ee282728bb5d3975ebc1b9e3b275e9084836a
priyanka_27 <dalwadipriyanka1727@gmail.com>
2024-09-01 12:09:04 +0530

    testAddBook testcase created

--- test/LibraryTest.java
+++ test/LibraryTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
  */
-
+import java.sql.*;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -17,119 +17,31 @@ import static org.junit.Assert.*;
  */
 public class LibraryTest {
     
-    public LibraryTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+      private Library library;
+
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+        library = new Library();
+        // Clean up database before each test
+        try (Connection conn = DatabaseHelper.getConnection();
+             Statement stmt = conn.createStatement()) {
+            stmt.execute("DELETE FROM borrowed_books");
+            stmt.execute("DELETE FROM books");
+            stmt.execute("DELETE FROM users");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    /**
-     * Test of addBook method, of class Library.
-     */
     @Test
     public void testAddBook() {
-        System.out.println("addBook");
-        Book book = null;
-        Library instance = new Library();
-        instance.addBook(book);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Book book = new Book("12345", "Test Book", "Test Author", 2023);
+        library.addBook(book);
+        
+        List<Book> availableBooks = library.getAvailableBooks();
+        assertEquals(1, availableBooks.size());
+        assertEquals(book.getIsbn(), availableBooks.get(0).getIsbn());
     }
 
-    /**
-     * Test of getAvailableBooks method, of class Library.
-     */
-    @Test
-    public void testGetAvailableBooks() {
-        System.out.println("getAvailableBooks");
-        Library instance = new Library();
-        List<Book> expResult = null;
-        List<Book> result = instance.getAvailableBooks();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of registerUser method, of class Library.
-     */
-    @Test
-    public void testRegisterUser() {
-        System.out.println("registerUser");
-        User user = null;
-        Library instance = new Library();
-        instance.registerUser(user);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of loginUser method, of class Library.
-     */
-    @Test
-    public void testLoginUser() {
-        System.out.println("loginUser");
-        String username = "";
-        String password = "";
-        Library instance = new Library();
-        User expResult = null;
-        User result = instance.loginUser(username, password);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of borrowBook method, of class Library.
-     */
-    @Test
-    public void testBorrowBook() {
-        System.out.println("borrowBook");
-        String isbn = "";
-        User user = null;
-        Library instance = new Library();
-        instance.borrowBook(isbn, user);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of returnBook method, of class Library.
-     */
-    @Test
-    public void testReturnBook() {
-        System.out.println("returnBook");
-        String isbn = "";
-        Library instance = new Library();
-        instance.returnBook(isbn);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of deleteBook method, of class Library.
-     */
-    @Test
-    public void testDeleteBook() throws Exception {
-        System.out.println("deleteBook");
-        String isbn = "";
-        Library instance = new Library();
-        instance.deleteBook(isbn);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
     
 }
